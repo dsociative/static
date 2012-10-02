5356da1878842270170c1a3ffd92c3824625e001
artem.prikhodin <admin@geektech.ru>
2012-10-02 18:59:29 +0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ setup(name='static loader',
       description='loader for json static data',
       author='dsociative',
       author_email='admin@geektech.ru',
-      packages=['static'],
-      package_dir={'static': 'static'},
+      packages=['static_loader'],
      )
